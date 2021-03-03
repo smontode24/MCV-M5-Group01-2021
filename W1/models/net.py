4295f9362185902493805216e97a12414b8c7840
Sergio <sergio.montoya@hertasecurity.com>
2021-03-03 23:50:30 +0100

    Pytorch model initialization

--- W1/models/net.py
+++ W1/models/net.py
@@ -43,6 +43,22 @@ class RandomNet:
         
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
+        self.init_layers()
+        
+    def init_layers(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # He initialization
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
     def __forward__(self, x):
         # Input block
         x = self.conv1(x)
